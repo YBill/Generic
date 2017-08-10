e97e84ee1e554c3d9b178c9f6bdf060281c6e4fc
weibiao <ybilland@163.com>
2017-08-10 11:08:38 +0800

    泛型函数

--- src/Test1.kt
+++ src/Test1.kt
@@ -6,6 +6,8 @@ fun main(args: Array<String>) {
     var arrayA: Array<A> = arrayOf(A(), A())
     var arrayB: Array<B> = arrayOf(B(), B())
     copy(arrayB, arrayA)
+
+    test(true)
 }
 
 open class A
@@ -14,4 +16,19 @@ class B : A()
 fun copy(from: Array<out A>, to: Array<in A>) {
     for (i in from.indices)
         to[i] = from[i]
+}
+
+fun <T> test(t: T) {
+    when (t) {
+        is Int -> {
+            println("this value is Int:" + t)
+        }
+        is String -> {
+            println("this value is String:" + t)
+        }
+        else -> {
+            println("this value is not define:" + t)
+        }
+
+    }
 }
